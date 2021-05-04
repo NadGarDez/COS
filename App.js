a2e4446962518eb6424ddedf4e4571f3b983617b
Nad GarDez <iranad@hotmail.com>
2021-05-04 23:22:23 +0000

    inicial

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +6,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Content from './content.js'
+
+export default function App() {
+  return (
+     <Content />
+  );
+}
+
